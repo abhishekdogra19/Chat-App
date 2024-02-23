0d609a63a45607d177301ecd9a54e0d2f3504bcd
abhishek dogra <abhidograaa19@gmail.com>
2024-02-24 02:58:51 +0530

    Implemented socket on the server for real-time messaging functionality.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const colors = require("colors");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const { Server } = require("socket.io");
 //db
 const connectDB = require("./db/connect");
 //routes
@@ -11,6 +12,7 @@ const userRouter = require("./routes/User");
 const chatRouter = require("./routes/Chat");
 const messageRouter = require("./routes/Message");
 const cookieParser = require("cookie-parser");
+
 //env
 const PORT = process.env.PORT || 6000;
 
@@ -36,8 +38,36 @@ app.use(errorHandler);
 const start = async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => {
+    const server = app.listen(PORT, () => {
       console.log(`Server is listening on PORT ${PORT}`.yellow);
+    });
+    const io = new Server(server, {
+      cors: {
+        origin: "http://localhost:5173",
+      },
+      pingTimeout: 60000, //The amount of time it will wait while being inactive.
+    });
+    io.on("connection", (socket) => {
+      socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        socket.emit("connection");
+      });
+
+      socket.on("join chat", (Room) => {
+        socket.join(Room);
+        console.log("Room joined ", Room);
+      });
+
+      socket.on("newMessage", (newMessageReceived) => {
+        console.log("New Message received ", newMessageReceived);
+        let chat = newMessageReceived.chat;
+        if (!chat.users) return console.log("Chat.users not defined");
+
+        chat.users.forEach((user) => {
+          if (user._id === newMessageReceived.sender._id) return;
+          socket.in(user._id).emit("message recieved", newMessageReceived);
+        });
+      });
     });
   } catch (error) {
     console.log(error);
